7537715501771d263d9f968ef09ddc179de9d002
satak <sami.koskivaara@gmail.com>
2019-03-03 11:36:30 +0200

    Add function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from flask import jsonify
 
 
+def modify_message(message):
+    if not message:
+        return 'Your message was empty'
+    return f'Your message was {message}'
+
+
 def main(request):
     message = request.args.get('message')
-    return jsonify({'message': message, 'version': 9})
+    return jsonify({'message': modify_message(message), 'version': 10})
