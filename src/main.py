0fadecd8f8fb50e273cee92dfb3269400fc4c7a6
satak <sami.koskivaara@gmail.com>
2019-03-03 10:41:55 +0200

    Version bump

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ from flask import jsonify
 
 def main(request):
     message = request.args.get('message')
-    return jsonify({'message': message})
+    return jsonify({'message': message, 'version': 1})
+
