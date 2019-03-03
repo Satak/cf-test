bcaa5ea3f27e0d32f034cba183dd0e09f2a551f2
satak <sami.koskivaara@gmail.com>
2019-03-03 13:53:41 +0200

    test token

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 from flask import jsonify
 import logging
+from os import getenv
 
 VERSION = 15
 APP_NAME = 'cf-test'
+TOKEN = getenv('TOKEN')
 
 
 def modify_message(message, request):
     if not message:
         logging.error(f'{APP_NAME}_APP_ERROR: Empty message. IP: {request.remote_addr}')
         return 'Your message was empty'
+    elif message == 'get_token':
+        return f'Token: {TOKEN}'
     logging.info(f'{APP_NAME}_APP_INFO: Message == {message}')
     return f'Your message was {message}'
 
